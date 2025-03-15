7ab0f1b34104615888749f1b15df1e3a014ab1ce
Marie-elizabeth Robert <121773867+mutaterx@users.noreply.github.com>
2025-03-15 21:42:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,7 @@ ticker = st.sidebar.text_input('Ticker')
 start_date = st.sidebar.date_input('Start Date')
 end_date = st.sidebar.date_input('End Date')
 data = yf.download(ticker,start=start_date, end=end_date)
+df = pd.DataFrame(data)
+
 st.plotly(data)
 st.write(data)
