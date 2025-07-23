91930709da00406120df69c88c6f21470d935aca
Marie-elizabeth Robert <121773867+mutaterx@users.noreply.github.com>
2025-07-23 19:21:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,29 +15,25 @@ st.markdown("<p style='font-size:15px; color:gray;'><em>Analyze the stock market
 st.markdown("<p style='font-size:15px; color:gray;'><em>https://www.linkedin.com/in/marie-elizabeth-robert-3181471a2/</em></p>", unsafe_allow_html=True)
 st.markdown("<p style='font-size:15px; color:white;'><em>Data is provided by Yahoo Finance</em></p>", unsafe_allow_html=True)
 
-
 # Using st.markdown with .sidebar, in order to add a description in the sidebar 
 st.sidebar.markdown("<p style='font-size:16px; color:gray;'>Enter the company symbol, start date, and end date to show data.</p>", unsafe_allow_html=True)
 
-# Now, in order to allows users to display data based on the companies they choose, I added an input section in the sidebar
+# Now, in order to allow users to display data based on the companies they choose, I added an input section in the sidebar
 # Users must choose a ticker (a ticker refers to the code used to represent a publicly traded company on a stock exchange)
-# For exemple Microsoft's ticker is MSFT
+# For example Microsoft's ticker is MSFT
 # Using the st.sidebar.text_input function to display an input box for the tickers
-# The variable is 'Ticker' and the default value is empty (value=""), so in order to display data, users must enter a ticker 
-# Because the default value is empty, no data will be displayed until a ticker is chosen
+# The variable is 'ticker' and the default value is empty (value=""), so in order to display data, users must enter a ticker 
 ticker = st.sidebar.text_input('Ticker', value="")
 
-# Using the date_input function so users can choose the periode of data to be displayed
+# Using the date_input function so users can choose the period of data to be displayed
 start_date = st.sidebar.date_input('Start Date')
 end_date = st.sidebar.date_input('End Date')
 
+# Adding a dropdown to switch between tabs for main content or company list
+tab = st.selectbox('Select a tab', ['Stock Data & Graph', 'Big Company Tickers'])
 
 # With this code, the fetched downloaded data and company info will be cached.
 # Caching the data will speed up the app and reduce the number of requests (in order to avoid Yahoo finance API limit)
-# Without caching, every time a user interacts with the app (like changing a date or ticker), it fetches data from the internet again. That can be slow, especially if the data source (Yahoo Finance) takes time to respond.
-# With caching, once data is downloaded once, Streamlit stores it locally and reuses it instantly on subsequent requests with the same inputs.
-# Services like Yahoo finance have rate limits (they limit how many requests you can send in a short period). If a user requests the same data repeatedly without caching, they risk hitting those limits and getting blocked temporarily.
-
 @st.cache_data(show_spinner=False)
 def get_stock_data(ticker_symbol, start, end):
     return yf.download(ticker_symbol, start=start, end=end)
@@ -46,23 +42,18 @@ def get_stock_data(ticker_symbol, start, end):
 def get_company_info(ticker_symbol):
     return yf.Ticker(ticker_symbol).info
 
-
 # If the tab variable has the value 'Stock Data & Graph' then view the code up until elif tab = 'Big Company Tickers'
 if tab == 'Stock Data & Graph':
-    # Before downloading the data from yahoo finance and displaying it, the code will check that the ticker variable is not empty (this avoids downloading errors)
-    # To check if the ticker variable has a value, I'll use "if ticker: data = yh ...." this translates to : if the ticker variable has a value/is not empty, then download data from yahoo finance
+    # Before downloading the data from Yahoo Finance and displaying it, the code will check that the ticker variable is not empty (this avoids downloading errors)
     if ticker:
-        data = yf.download(ticker, start=start_date, end=end_date)
-
-        # Creating the list tickers that stores all the ticker symboles entered by users
-        # In case users enter commas to seperate the tickers, the code will trasform the commas into spaces with ticker.replace(',',' ').
-        # It will look like this : INPUT --> AAPL, GOOG, MSFT     CODE --> ticker.replace(',', ' ')    OUTPUT-->  "AAPL  GOOG MSFT"
-        # Then the code will store the values into a list thanks to .split(), which looks like this : "AAPL  GOOG MSFT".split() -->  ["AAPL", "GOOG", "MSFT"]
-        # for t in ticker, is a loop, in the code below the code is looping over each symbol that was stored in the created list
-        # So it looks like this : for t in ["AAPL", "GOOG", "MSFT"]
+        # Creating the list 'tickers' that stores all the ticker symbols entered by users
+        # In case users enter commas to separate the tickers, the code will transform the commas into spaces with ticker.replace(',', ' ')
+        # Then the code will store the values into a list thanks to .split()
+        # For example: "AAPL, MSFT, TSLA" → ["AAPL", "MSFT", "TSLA"]
         tickers = [t.strip() for t in ticker.replace(',', ' ').split()]
-        # len(tickers) checks the number of items in the list named tickers that holds the stock symbols
-        # if and > 0, allows the code to be run if there is at least 1 symbol in the list
+
+        # len(tickers) checks the number of items in the list named 'tickers'
+        # if and > 0, allows the code to run if there is at least 1 symbol in the list
         if len(tickers) > 0:  # Check if there are valid tickers
             data = yf.download(tickers, start=start_date, end=end_date)
 
@@ -83,14 +74,36 @@ if tab == 'Stock Data & Graph':
                     except KeyError:
                         st.warning(f"Data for {tick} not available")
 
+                # Display the line chart for closing prices
                 fig.update_layout(
                     title="Closing Prices",
                     xaxis_title="Date",
                     yaxis_title="Price",
                     legend_title="Tickers"
                 )
+                st.plotly_chart(fig, use_container_width=True)
+
+            else:
+                # If it's a single ticker, handle it directly
+                fig = go.Figure()
+                fig.add_trace(go.Scatter(
+                    x=data.index,
+                    y=data['Close'],
+                    mode='lines',
+                    name=f"{tickers[0]} Close"
+                ))
+                fig.update_layout(
+                    title=f"{tickers[0]} - Closing Price",
+                    xaxis_title="Date",
+                    yaxis_title="Price"
+                )
+                st.plotly_chart(fig, use_container_width=True)
+
+            # Display the raw data table
+            st.subheader('Stock Data')
+            st.dataframe(data)
         else:
-            st.warning("Please enter a valid ticker symbol.") # If the ticker is not valid, then users will recieve a message asking them to enter a valid ticker symbol
+            st.warning("Please enter a valid ticker symbol.")  # If the ticker is not valid, show warning
     else:
         st.warning("Please enter a ticker symbol.")
 
@@ -109,7 +122,7 @@ elif tab == 'Big Company Tickers':
         'Walmart': 'WMT'
     }
 
-    # Using st.subheader and st.srite to write the subtitle and description of the dictionary tab 
+    # Using st.subheader and st.write to write the subtitle and description of the dictionary tab 
     st.subheader('Big Companies and Tickers')
     st.write("Here are some big companies and their ticker symbols:")
 
@@ -118,4 +131,3 @@ elif tab == 'Big Company Tickers':
 
     # Add a clickable link to bring users to a dictionary of tickers (external link)
     st.markdown("[Click here for a full dictionary of tickers](https://www.nasdaq.com/symbol/)")
-
